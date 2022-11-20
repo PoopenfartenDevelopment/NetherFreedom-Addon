f82c9c242892938b3e886bb09a16292a52e82e97
Gnome <85032707+TrashmanEatGarbage@users.noreply.github.com>
2022-11-19 19:27:15 -0800

    pickaxe getter is finally fixed

--- src/main/java/netherfreedom/modules/main/BaritoneMinerRewrite.java
+++ src/main/java/netherfreedom/modules/main/BaritoneMinerRewrite.java
@@ -24,9 +24,17 @@ import meteordevelopment.meteorclient.utils.world.BlockUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.screen.DisconnectedScreen;
+import net.minecraft.client.gui.screen.ingame.ShulkerBoxScreen;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import netherfreedom.modules.NetherFreedom;
+import netherfreedom.modules.kmain.NFNuker;
 
 public class BaritoneMinerRewrite extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
@@ -59,6 +67,22 @@ public class BaritoneMinerRewrite extends Module {
             .build()
     );
 
+    private final Setting<Integer> shulkerSlot = sgGeneral.add(new IntSetting.Builder()
+            .name("dedicated-shulker-slot")
+            .description("put a shulker in this slot and it will place the shulker to refill")
+            .defaultValue(0)
+            .range(0,9)
+            .sliderRange(0,9)
+            .build()
+    );
+
+    private final Setting<Boolean> saveLineProgress = sgGeneral.add(new BoolSetting.Builder()
+            .name("save line progress")
+            .description("saves the progress on the current line you're digging")
+            .defaultValue(true)
+            .build()
+    );
+
     private final Setting<Boolean> renderCorners = sgGeneral.add(new BoolSetting.Builder()
             .name("render-corners")
             .description("renders the 2 corners.")
@@ -73,10 +97,10 @@ public class BaritoneMinerRewrite extends Module {
             .build()
     );
 
-    private final Setting<Boolean> pathToStart = sgGeneral.add(new BoolSetting.Builder()
-            .name("path to start")
-            .description("paths to corner 1 before beginning")
-            .defaultValue(false)
+    private final Setting<Boolean> getPickaxe = sgGeneral.add(new BoolSetting.Builder()
+            .name("get-pickaxe-from-skulker")
+            .description("if it runs out of pickaxes it will get pickaxes from a shulker")
+            .defaultValue(true)
             .build()
     );
 
@@ -97,10 +121,9 @@ public class BaritoneMinerRewrite extends Module {
         super(NetherFreedom.MAIN, "BaritoneMinerRewrite", "this shit bugs me");
     }
 
-    private BlockPos endOfLinePos, barPos, offsetPos, currPlayerPos = null;
-    private Direction toEndOfLineDir, toAdvanceDir = null;
-    private boolean offsetting = false;
-    private boolean reachedStart = true;
+    private BlockPos endOfLinePos, barPos, offsetPos, currPlayerPos, shulkerPlacePos, savedPos = null;
+    private Direction toEndOfLineDir, toAdvanceDir, shulkerPlaceDir = null;
+    private boolean offsetting, refilling, placedShulker = false;
     private int length,dist = 0;
 
     @Override
@@ -113,18 +136,14 @@ public class BaritoneMinerRewrite extends Module {
         endOfLinePos = currPlayerPos.offset(toEndOfLineDir, length);
         barPos = currPlayerPos.offset(toEndOfLineDir,2);
         setGoal(barPos);
+
         if (enableDT.get()){
             if (!modules.isActive(DiggingTools.class)){
                 modules.get(DiggingTools.class).toggle();
             }
         }
 
-        if (pathToStart.get()){
-            reachedStart = false;
-            setGoal(cornerOne.get());
-        }else{
-            reachedStart = true;
-        }
+        shulkerPlaceDir = toEndOfLineDir.getOpposite();
 
         baritoneSettings.blockPlacementPenalty.value = 0.0;
         baritoneSettings.assumeWalkOnLava.value = true;
@@ -141,7 +160,9 @@ public class BaritoneMinerRewrite extends Module {
         toAdvanceDir = null;
         offsetting = false;
         length = 0;
-        reachedStart = false;
+        shulkerPlaceDir = toEndOfLineDir.getOpposite();
+        placedShulker = false;
+        refilling = false;
 
         if (enableDT.get()){
             if (modules.isActive(DiggingTools.class)){
@@ -152,9 +173,8 @@ public class BaritoneMinerRewrite extends Module {
     }
 
     @EventHandler
-     public void onTick(TickEvent.Pre event){
+     public void onTick(TickEvent.Pre event) throws InterruptedException {
         currPlayerPos = mc.player.getBlockPos();
-
         if (pauseBind.get().isPressed()) {
             if(baritone.getPathingBehavior().isPathing()){
                 baritone.getCommandManager().execute("pause");
@@ -162,12 +182,54 @@ public class BaritoneMinerRewrite extends Module {
                 baritone.getCommandManager().execute("resume");
             }
         }
-
-        if (currPlayerPos.equals(cornerOne.get())){
-            reachedStart = true;
+        if (notHavePickaxe() && !placedShulker && getPickaxe.get()) {
+            baritone.getCommandManager().execute("pause");
+            refilling = true;
+            GoalBlock baritoneGoal = (GoalBlock) baritone.getCustomGoalProcess().getGoal();
+            savedPos = new BlockPos(baritoneGoal.x,baritoneGoal.y,baritoneGoal.z);
+            info("ran out of pickaxes... refilling");
+            shulkerPlacePos = currPlayerPos.offset(shulkerPlaceDir, 2);
+            if (modules.get(NFNuker.class).isActive()) modules.get(NFNuker.class).toggle();
+            if (BlockUtils.place(shulkerPlacePos, Hand.MAIN_HAND, shulkerSlot.get(), true, 0, true, true, false)) {
+                placedShulker = true;
+            }else{
+                info("unable to place... redirecting");
+                shulkerPlaceDir = shulkerPlaceDir.rotateYClockwise();
+                placedShulker = false;
+                shulkerPlacePos = null;
+                return;
+            }
+            return;
         }
 
-        if (!currPlayerPos.equals(barPos) && !offsetting && reachedStart) {
+        if (notHavePickaxe() && placedShulker) {
+            BlockHitResult bhr = new BlockHitResult(Vec3d.ofCenter(shulkerPlacePos, 1), Direction.UP, shulkerPlacePos, false);
+            mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, bhr);
+            if (mc.currentScreen instanceof ShulkerBoxScreen) {
+                if (grabAllPickaxes() == 0){
+                    mc.currentScreen.close();
+                    shulkerPlaceDir = shulkerPlaceDir.rotateYClockwise();
+                    placedShulker = false;
+                    shulkerPlacePos = null;
+                    return;
+                }
+                mc.currentScreen.close();
+            }
+            setGoal(shulkerPlacePos);
+            baritone.getCommandManager().execute("resume");
+        }
+
+        if (currPlayerPos.equals(shulkerPlacePos)) {
+
+
+            setGoal(savedPos);
+            shulkerPlacePos = null;
+            savedPos = null;
+            placedShulker = false;
+            refilling = false;
+        }
+
+        if (!currPlayerPos.equals(barPos) && !offsetting && !refilling) {
             try {
                 BlockPos preBarPos = new BlockPos(barPos.offset(toEndOfLineDir.getOpposite()));
                 if (currPlayerPos.equals(preBarPos)) {
@@ -220,9 +282,9 @@ public class BaritoneMinerRewrite extends Module {
     private void onRender(Render3DEvent event){
         if (renderCorners.get()) {
             try {
-                Color DarkRed = new Color(139,0,0);
+                Color DARKRED = new Color(139,0,0);
                 event.renderer.box(cornerOne.get(), Color.RED,Color.RED, ShapeMode.Both,0);
-                event.renderer.box(cornerTwo.get(),DarkRed,DarkRed, ShapeMode.Both,0);
+                event.renderer.box(cornerTwo.get(),DARKRED,DARKRED, ShapeMode.Both,0);
                 event.renderer.box(endOfLinePos,Color.BLUE,Color.BLUE,ShapeMode.Both,0);
             } catch(Exception ignored){}
         }
@@ -265,4 +327,38 @@ public class BaritoneMinerRewrite extends Module {
         return dist;
     }
 
+//pickaxe refilling stuff
+    private boolean notHavePickaxe() {
+        for (int i = 0; i < mc.player.getInventory().main.size(); i++) {
+            if (mc.player.getInventory().getStack(i).getItem() == Items.NETHERITE_PICKAXE) return false;
+        }
+        return true;
+    }
+
+    //double-clicks on slot if it has a pickaxe stops when it has only 1 slot available to leave room to pick up the shulker box
+    private int grabAllPickaxes(){
+        int picksMoved = 0;
+        int availableSlots = 0;
+
+        for (int i = 27; i < mc.player.currentScreenHandler.slots.size(); i++ ){
+            Item item = mc.player.currentScreenHandler.getSlot(i).getStack().getItem();
+            if(item.equals(Items.AIR)){
+                availableSlots++;
+            }
+        }
+        info("availableSlots: " + availableSlots);
+
+        for (int i = 0; i < mc.player.currentScreenHandler.slots.size() - 36; i++) {
+            Item item = mc.player.currentScreenHandler.getSlot(i).getStack().getItem();
+            if(item.equals(Items.NETHERITE_PICKAXE)){
+                //do not fucking ask why
+                if(availableSlots - 2 > picksMoved){
+                    mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, i, 1, SlotActionType.QUICK_MOVE, mc.player);
+                    picksMoved++;
+                }
+            }
+        }
+        info("picksMoved: " + picksMoved);
+        return picksMoved;
+    }
 }
