b8a377f2ff615042fb3c60969a038f2b81203c36
Gnome <85032707+TrashmanEatGarbage@users.noreply.github.com>
2022-11-21 10:23:09 -0800

    this cool thing

--- src/main/java/netherfreedom/modules/main/DiscordRPC.java
+++ src/main/java/netherfreedom/modules/main/DiscordRPC.java
@@ -74,7 +74,7 @@ public class DiscordRPC extends Module {
     private final Setting<List<String>> line2Strings = sgLine2.add(new StringListSetting.Builder()
             .name("line-2-messages")
             .description("Messages used for the second line.")
-            .defaultValue("{player}", "Actively digging", "{server.player_count} Players online")
+            .defaultValue("have broken {player.get_stat(\"netherrack\",\"mined\")} netherrack", "Actively digging", "{server.player_count} Players online")
             .onChanged(strings -> recompileLine2())
             .build()
     );
